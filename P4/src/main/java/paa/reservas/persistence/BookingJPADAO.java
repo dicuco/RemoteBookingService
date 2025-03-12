8ec3e8bdf4a042496e1b78d926a3ed1d7af4e0ff
DiegoCC <53096099+dicuco@users.noreply.github.com>
2025-03-12 12:21:28 +0100

    Update BookingJPADAO.java
    remove comments

--- P4/src/main/java/paa/reservas/persistence/BookingJPADAO.java
+++ P4/src/main/java/paa/reservas/persistence/BookingJPADAO.java
@@ -16,36 +16,7 @@ public class BookingJPADAO extends JPADAO<Booking,Long>{
 		super(em, Booking.class);
 		// TODO Auto-generated constructor stub
 	}
-	//hacer un metodo para saber que habitaciones estan ocupadas en un periodo de tiempo
-	//que devuelva los numeros de las habitaciones ocupadas
-//	public List<Integer> findindRooms(Long hotelCode, LocalDate date) {
-//		TypedQuery<Integer> q = em.createQuery(
-//		        "SELECT b.roomNumber FROM Booking b " +
-//			    "WHERE b.hotel.code = :hotelCode " +
-//		        "WHERE b.departureDate > :date " +
-//		        "AND (b.arrivalDate < :date OR b.arrivalDate = :date) " +
-//		        "AND b.roomNumber <= :MAXIMUM_SINGLE_ROOM_NUMBER", Integer.class);
-//		q.setParameter("date", date);
-//	    q.setParameter("hotelCode", hotelCode);
-//		q.setParameter("MAXIMUM_SINGLE_ROOM_NUMBER", 199);
-//		List<Integer> result=q.getResultList();
-//		int a=result.size();
-//		return result;
-//	}
-//	public int findindRooms(Long hotelCode, LocalDate date) {
-//		TypedQuery<Integer> q = em.createQuery(
-//		        "SELECT b.roomNumber FROM Booking b " +
-//			    "WHERE b.hotel.code = :hotelCode " +
-//		        "WHERE b.departureDate > :date " +
-//		        "AND (b.arrivalDate < :date OR b.arrivalDate = :date) " +
-//		        "AND b.roomNumber <= :MAXIMUM_SINGLE_ROOM_NUMBER", Integer.class);
-//		q.setParameter("date", date);
-//	    q.setParameter("hotelCode", hotelCode);
-//		q.setParameter("MAXIMUM_SINGLE_ROOM_NUMBER", 199);
-//		List<Integer> result=q.getResultList();
-//		int a=result.size();
-//		return a;
-//	}
+
 	public List<Integer> findNumberRooms(Long hotelCode, LocalDate arrivalDate, LocalDate departureDate) {
 	    TypedQuery<Integer> q = em.createQuery(
 	            "SELECT b.roomNumber FROM Booking b " +
